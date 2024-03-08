fe8554e4bebef53b4949ad02aa52632dcf969849
mbkorecki <mbokorecki@gmail.com>
2024-03-09 00:13:34 +0100

    training

--- inference.py
+++ inference.py
@@ -1,9 +1,19 @@
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit= True,
+    bnb_4bit_quant_type= "nf4",
+    bnb_4bit_compute_dtype= torch.bfloat16,
+    bnb_4bit_use_double_quant= False,
+)
+
+
+
 model = AutoModelForCausalLM.from_pretrained(
 
-    "land_mistral/model", device_map="auto", load_in_4bit=True
+    "land_mistral/model", device_map="auto", quantization_config=bnb_config
 
 )
 
