302ae98e0a45da7c21d029b02e86fce675a94612
mbkorecki <mbokorecki@gmail.com>
2024-03-13 12:30:21 +0100

    trying to chat fine-tune

--- mistral_finetune.py
+++ mistral_finetune.py
@@ -1,9 +1,11 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig,HfArgumentParser,TrainingArguments,pipeline, logging, TextStreamer
 from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training, get_peft_model
-import os, torch, wandb, platform, warnings
+import os, torch, platform, warnings
 from datasets import load_dataset, load_from_disk
 from trl import SFTTrainer
 from huggingface_hub import notebook_login
+
+
 #Use a sharded model to fine-tune in the free version of Google Colab.
 base_model = "mistralai/Mistral-7B-v0.1" #bn22/Mistral-7B-Instruct-v0.1-sharded
 dataset_name, new_model = "gathnex/Gath_baize", "gathnex/Gath_mistral_7b"
@@ -11,7 +13,6 @@ dataset_name, new_model = "gathnex/Gath_baize", "gathnex/Gath_mistral_7b"
 # Loading a Gath_baize dataset
 dataset = load_dataset(dataset_name, split="train")
 # dataset = load_from_disk("data/chat_capital_dataset")  # use this one for local dataset!
-dataset["chat_sample"][0]
 
 model = AutoModelForCausalLM.from_pretrained(base_model, device_map="auto")
 
