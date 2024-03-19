9895b180bdba023eed78ef1f856ba6ea296fd697
mbkorecki <mbokorecki@gmail.com>
2024-03-19 15:46:16 +0100

    finetune full param

--- fine_tune.py
+++ fine_tune.py
@@ -28,7 +28,7 @@ peft_config = LoraConfig(
         lora_dropout=0.05,
         bias="none",
         task_type="CAUSAL_LM",
-        target_modules="all-linear"
+        target_modules=["all-linear", "q_proj", "k_proj", "v_proj", "o_proj","gate_proj"]
     )
         #target_modules=["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"]
 
